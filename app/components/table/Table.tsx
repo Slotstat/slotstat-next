3d780baee2880df3695ea22b40a6a58638b9e114
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2023-07-18 16:42:22 +0200

    fix type error

--- app/components/table/Table.tsx
+++ app/components/table/Table.tsx
@@ -39,7 +39,7 @@ type Props = {
   }: GetGamesFromChosenCasinoProps) => void;
   setSearchKeyInBottomSheet?: (text: string) => void;
   setOrderByKeyInBottomSheet?: (text: string | undefined) => void;
-  showCryptoFiatSwitcher: boolean;
+  showCryptoFiatSwitcher?: boolean;
 };
 
 const Table = ({
