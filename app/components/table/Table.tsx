af8475856fbe54a730a25676818ea779ffada128
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2023-09-26 22:41:36 +0400

    remove console

--- app/components/table/Table.tsx
+++ app/components/table/Table.tsx
@@ -58,7 +58,6 @@ const Table = ({
   showCryptoFiatSwitcher,
   setIsFiatState,
 }: Props) => {
-  console.log("tableBodyData", tableBodyData);
   const t = useTranslations("table");
   const f = useTranslations();
   const { setQueryParams } = useQueryParams();
