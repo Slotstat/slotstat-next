5aa795d13571c73b244b29c2c2c8e20918026fc2
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2024-02-28 22:59:48 +0400

    inside RTP

--- app/components/chart/RTP.tsx
+++ app/components/chart/RTP.tsx
@@ -132,17 +132,23 @@ export default function RTP({
                 fontSize="12"
                 fontFamily="modernist"
               >
-                {RTP === gameObject.rtp.preferredValue
-                  ? neutral
-                  : RTP > gameObject.rtp.preferredValue
-                  ? slotIsLoosing +
-                    " " +
-                    (RTP - gameObject.rtp.preferredValue).toFixed(2) +
-                    "%"
-                  : slotIsInProfit +
-                    " " +
-                    (gameObject.rtp.preferredValue - RTP).toFixed(2) +
-                    "%"}
+                {RTP === gameObject.rtp.preferredValue ? (
+                  neutral
+                ) : RTP > gameObject.rtp.preferredValue ? (
+                  <>
+                    {slotIsLoosing}{" "}
+                    <tspan fill="#fff">
+                      {(RTP - gameObject.rtp.preferredValue).toFixed(2)}%
+                    </tspan>
+                  </>
+                ) : (
+                  <>
+                    {slotIsInProfit}{" "}
+                    <tspan fill="#fff">
+                      {(gameObject.rtp.preferredValue - RTP).toFixed(2)}%
+                    </tspan>
+                  </>
+                )}
               </text>
               <defs>
                 <linearGradient
