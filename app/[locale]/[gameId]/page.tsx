5aa26c8961699fa633e06f7affcd3aacd4e2ab8c
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2024-06-29 13:59:59 +0400

    test 19

--- app/[locale]/[gameId]/page.tsx
+++ app/[locale]/[gameId]/page.tsx
@@ -69,7 +69,7 @@ export default async function gamePage({
   let compareGame;
 
   const mainGameData: Promise<GameData> = getSingleGame(gameId);
-  const gamesCardsData: Promise<Card[]> = getGameCards(locale, gameId);
+  // const gamesCardsData: Promise<Card[]> = getGameCards(locale, gameId);
 
   // const casinoData: Promise<CasinoData> = getCasino(casId);
   // const casinoCardsData: Promise<Card[]> = getCasinoCards(locale, casId);
@@ -80,7 +80,7 @@ export default async function gamePage({
   //   gamesCardsData,
   // ]);
   const mainGame = await mainGameData;
-  const gameCards = await gamesCardsData;
+  // const gameCards = await gamesCardsData;
 
   // const casino = await casinoData;
   // const casinoCards = await casinoCardsData;
