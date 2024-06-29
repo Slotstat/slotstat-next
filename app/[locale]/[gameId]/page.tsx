3140c22099ef7dfb7ae8f62e973309d9051489df
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2024-06-29 12:07:49 +0400

    test2

--- app/[locale]/[gameId]/page.tsx
+++ app/[locale]/[gameId]/page.tsx
@@ -46,7 +46,7 @@ export async function generateMetadata({
 }
 
 const wait = () => {
-  return new Promise((resolve, reject) => setTimeout(resolve, 1000));
+  return new Promise((resolve, reject) => setTimeout(resolve, 3000));
 };
 
 export default async function gamePage({
