1386e6e0daef7124a9b567513009493063d6552d
levankh14 <levankh14@gmail.com>
2024-08-20 13:43:00 +0400

    apiKey env variable

--- lib/clientSide/chatGPT/chatBotApiRequests.tsx
+++ lib/clientSide/chatGPT/chatBotApiRequests.tsx
@@ -6,7 +6,8 @@ import { deleteCookie } from "cookies-next";
 import { OptionsType } from "cookies-next/lib/types";
 import { cookies } from "next/headers";
 
-const bearer = `Bearer sk-k4uT6i_50f1G6is_rOg--sqF6FW7Jzpns_sopgAYuST3BlbkFJe3HX2ZlXIuOYdI3vAqG3B_NobuA24EnuaweT_wHhUA`;
+// const bearer = `Bearer sk-k4uT6i_50f1G6is_rOg--sqF6FW7Jzpns_sopgAYuST3BlbkFJe3HX2ZlXIuOYdI3vAqG3B_NobuA24EnuaweT_wHhUA`;
+const bearer = `Bearer ${process.env.OPENAI_API_KEY}`;
 export async function createThread(
   setThreadId: React.Dispatch<React.SetStateAction<string | undefined>>,
   setCookie: (name: string, value: string) => void
