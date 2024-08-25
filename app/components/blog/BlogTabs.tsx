7f9b418bbd84a67afcc28aedc08c160ce9baf7aa
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2024-08-25 19:05:46 +0400

    blog fix

--- app/components/blog/BlogTabs.tsx
+++ app/components/blog/BlogTabs.tsx
@@ -56,7 +56,7 @@ export default function BlogTabs({
   return (
     <>
       <div
-        className={`flex justify-center bg-dark1/90 w-[100%]  ${scrolled && "fixed top-0 z-10 left-0 right-0 py-3 md:py-0"}`}
+        className={`flex justify-center bg-dark1/90 w-[100%]  ${scrolled && "fixed top-0 z-10 left-0 right-0 py-3 px-4 md:px-0 md:py-0"}`}
       >
         <div className="max-w-screen-xl w-[100%] md:flex  md:justify-between ">
           <div
