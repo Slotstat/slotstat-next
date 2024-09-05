e9b9e4ffcb2a83080b8f6e8dc6925142f071d0fa
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2024-09-05 18:53:16 +0400

    test cloudeflare region

--- middleware.ts
+++ middleware.ts
@@ -10,21 +10,16 @@ export default async function middleware(request: NextRequest) {
 
   const country = geo.country || "GE";
 
-  // const city = geo.city || "San Francisco";
   const region = geo.region || "TB";
+  // const city = geo.city || "San Francisco";
 
   const countryInfo = countries.find((x) => x.cca2 === country);
 
-  // const currencyCode = Object.keys(countryInfo.currencies)[0];
-  // const currency = countryInfo.currencies[currencyCode];
-  // const languages = Object.values(countryInfo.languages).join(", ");
-
-
-  const country1 = request.headers.get('CF-IPCountry')
+  const country1 = request.headers.get("CF-IPCountry");
+  const region1 = request.headers.get("CF-IPRegion") || "unknown";
 
   // Step 1: Use the incoming request
-  const defaultLocale =
-    request.headers.get("x-default-locale") || "en";
+  const defaultLocale = request.headers.get("x-default-locale") || "en";
 
   // Step 2: Create and call the next-intl middleware
   const handleI18nRouting = createIntlMiddleware({
@@ -51,10 +46,9 @@ export default async function middleware(request: NextRequest) {
     region && response.cookies.set("region", region);
   }
   country && response.cookies.set("currentLocCountry", country);
-    region && response.cookies.set("currentLocRegion", region);
+  region && response.cookies.set("currentLocRegion", region);
 
   // city && response.cookies.set("city", city);
-
 
   // response.headers.set("country", country);
   // response.headers.set("city", city);
@@ -64,7 +58,8 @@ export default async function middleware(request: NextRequest) {
   // response.headers.set("name", currency.name);
   // response.headers.set("languages", languages);
   // Step 3: Alter the response
-  response.headers.set('X-User-Country', country1 || 'unknown')
+  response.headers.set("X-User-Country", country1 || "unknown");
+  response.headers.set("X-User-region", region1 || "unknown");
   response.headers.set("x-default-locale", defaultLocale);
   NextResponse.rewrite(url);
   return response;
